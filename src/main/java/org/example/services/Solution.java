e890d2dd0ccdbb02802439c1588bf800c33593e7
rubendario2110 <43899591+rubendario2110@users.noreply.github.com>
2025-04-14 12:47:10 -0500

    ♻️ refactoring services

--- src/main/java/org/example/services/Solution.java
+++ src/main/java/org/example/services/Solution.java
@@ -1,26 +1,32 @@
 package org.example.services;
 
 public class Solution {
-    public int numDistinct(String s, String t) {
-        int m = s.length();
-        int n = t.length();
+    public int numDistinct(String source, String target) {
+        int sourceLength = source.length();
+        int targetLength = target.length();
 
-        if (n > m)
-            return 0;
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+        if (targetLength > sourceLength) return 0;
+
+        int[][] dp = initializeDPMatrix(sourceLength, targetLength);
+
+        for (int i = 1; i <= sourceLength; i++) {
+            for (int j = 1; j <= targetLength; j++) {
+                if (source.charAt(i - 1) == target.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
-
             }
         }
-        return dp[m][n];
+
+        return dp[sourceLength][targetLength];
+    }
+
+    private int[][] initializeDPMatrix(int rows, int cols) {
+        int[][] dp = new int[rows + 1][cols + 1];
+        for (int i = 0; i <= rows; i++) {
+            dp[i][0] = 1;
+        }
+        return dp;
     }
 }
